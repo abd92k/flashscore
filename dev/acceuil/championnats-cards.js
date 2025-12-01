44a5fbb9f35ddc7fd96dc03cf4d8763c6046afe0
Amon92z <davasgaiumamon@gmail.com>
2025-12-01 17:32:55 +0100

    changement
    amélioration

--- dev/acceuil/championnats-cards.js
+++ dev/acceuil/championnats-cards.js
@@ -1,14 +1,13 @@
-// championnats-cards.js
-// Quand on clique sur une card, on redirige vers championnat.html avec un paramètre
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const cards = document.querySelectorAll(".champ-card");
 
   cards.forEach(card => {
     card.addEventListener("click", () => {
-      const key = card.dataset.key; // ex: "premier_league", "la_liga", etc.
+      const key = card.dataset.key; 
 
-      // On envoie la clé dans l'URL, on lira ça dans championnat.html
+
       const url = `championnats.html?champ=${encodeURIComponent(key)}`;
       window.location.href = url;
     });
